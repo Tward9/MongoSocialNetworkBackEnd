7f6a3c62e485864f1b39927c9b236d3e9bfefd7a
Theo Ward <theoward9@gmail.com>
2022-06-05 11:44:41 -0400

    created user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUsers).post(createUser);
+
+// /api/user/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
